903b4473ea02324b3ecb9ad31b81392bb7e10a3a
Lilian Burdianov <lilian.burdianov@gmail.com>
2021-06-06 21:45:54 +0400

    implement user authorization and jwt token generation

--- src/auth/auth.constants.ts
+++ src/auth/auth.constants.ts
@@ -1,2 +1,4 @@
 export const ALREADY_REGISTERED_ERROR =
   'Такой пользователь уже был зарегистрирован';
+export const USER_NOT_FOUND_ERROR = 'Пользователь с таким email не найден';
+export const WRONG_CREDENTIALS_ERROR = 'Неправильные данные';
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TypegooseModule } from 'nestjs-typegoose';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 import { AuthController } from './auth.controller';
 import { UserModel } from './user.model';
 import { AuthService } from './auth.service';
+import { getJwtConfig } from './../configs/jwt.config';
 
 @Module({
   controllers: [AuthController],
@@ -15,7 +18,12 @@ import { AuthService } from './auth.service';
           collection: 'User'
         }
       }
-    ])
+    ]),
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: getJwtConfig
+    })
   ],
   providers: [AuthService]
 })
